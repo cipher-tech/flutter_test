363523c39fc2521197d180f90d106d9f46b60c08
cipher-tech <nickchibuikem@gmail.com>
2023-06-06 10:30:55 +0100

    added database

--- database/index.js
+++ database/index.js
@@ -1,37 +1,124 @@
-import Sequelize from "sequelize";
-import config from "../config/index";
-import makeModels from "./models";
-
-const { DATABASE,
-    USERNAME,
-    PASSWORD,
-    HOST,
-    DATABASE_PORT,
-    DIALECT } = config;
-
-const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
-    host: HOST,
-    dialect: DIALECT,
-    port: DATABASE_PORT,
-    operatorsAliases: false,
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 3000,
-        idle: 0
-    }
-});
-
-const database = (DatabaseConfig, sequelizeInstance) => {
-    const model = {};
-
-    model.Sequelize = Sequelize;
-    model.sequelize = sequelize;
-
-    const modelInstances = makeModels(DatabaseConfig, sequelizeInstance);
-    Object.assign(model, { ...modelInstances });
-    return model;
-};
-
-export default database(sequelize, Sequelize);
+export const chats = {
+    all: [
+        {
+            "id": 1,
+            "last_message": "Set a time for us",
+            "members": ["joseph", "fredrick", "cipher", "lily"],
+            "topic": "code hangouts",
+            "modified_at": 15778332000200,
+        },
+        {
+            "id": 2,
+            "last_message": "We need a new endpoint for the feature set",
+            "members": ["dayo", "rotimi"],
+            "topic": "field operations",
+            "modified_at": 1577833200063,
+        },
+        {
+            "id": 3,
+            "last_message": "I need to be back by Wednesday",
+            "members": ["joseph"],
+            "topic": null,
+            "modified_at": 1577833204000,
+        },
+        {
+            "id": 4,
+            "last_message": "Maybe?",
+            "members": ["lily"],
+            "topic": null,
+            "modified_at": 1577833201100,
+        },
+    ],
+    1: [
+        {
+            "id": 1,
+            "chat_id": 1,
+            "sender": "joseph",
+            "message": "We are setting a time for the meet",
+            "modified_at": 15778332000199,
+        },
+        {
+            "id": 2,
+            "chat_id": 1,
+            "sender": "cipher",
+            "message": "I have fixed all outstanding issues",
+            "modified_at": 15778332000189,
+        },
+        {
+            "id": 3,
+            "chat_id": 1,
+            "sender": "lily",
+            "message": "No issues, we'll make it happen",
+            "modified_at": 15778332000178,
+        },
+    ],
+    2: [
+        {
+            "id": 1,
+            "chat_id": 2,
+            "sender": "dayo",
+            "message": "What are the doc requirements?",
+            "modified_at": 1577833200023,
+        },
+        {
+            "id": 2,
+            "chat_id": 2,
+            "sender": "rotimi",
+            "message": "Samples for the underwriting module is running now",
+            "modified_at": 1577833200053,
+        },
+        {
+            "id": 3,
+            "chat_id": 2,
+            "sender": "rotimi",
+            "message": "I have generated it",
+            "modified_at": 1577833200060,
+        },
+    ],
+    3: [
+        {
+            "id": 1,
+            "chat_id": 3,
+            "sender": "joseph",
+            "message": "Make an arrangement",
+            "modified_at": 1577833203800,
+        },
+        {
+            "id": 2,
+            "chat_id": 3,
+            "sender": "joseph",
+            "message": "We can sync by 7:00PM EST",
+            "modified_at": 1577833203830,
+        },
+        {
+            "id": 3,
+            "chat_id": 3,
+            "sender": "joseph",
+            "message": "I'll fly in soonest",
+            "modified_at": 1577833203840,
+        },
+    ],
+    4: [
+        {
+            "id": 1,
+            "chat_id": 4,
+            "sender": "lily",
+            "message": "Are you sure you can continue this way?",
+            "modified_at": 1577833201020,
+        },
+        {
+            "id": 2,
+            "chat_id": 4,
+            "sender": "lily",
+            "message": "I have doubts of this endeavour",
+            "modified_at": 1577833201050,
+        },
+        {
+            "id": 3,
+            "chat_id": 4,
+            "sender": "lily",
+            "message": "I promise you",
+            "modified_at": 1577833201090,
+        },
+    ]
+}
